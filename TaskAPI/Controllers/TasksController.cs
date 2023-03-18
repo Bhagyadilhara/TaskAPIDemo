4d475dc6e4bba1cd037f0697d12278ac58b39107
Bhagya Dilhara <bhagya.d@sits.lk>
2023-03-18 13:46:36 +0530

    Add project files.

---
+++ TaskAPI/Controllers/TasksController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet] //method level attribute
+        public IActionResult Tasks()     //can send response with status codes by using IActionResult type
+        {
+            var tasks = new string[] { "Task 1", "Task 2", "Task 3" };
+            return Ok(tasks); 
+        }
+
+        [HttpPost]
+        public IActionResult NewTask()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTask()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTask()
+        {
+            var somethingWentWrong = true;
+            if (somethingWentWrong)
+                return BadRequest();
+            return Ok();
+        }
+    }
+}
