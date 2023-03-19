5df0ffed285966a35286f7c051e3ed1d9b4dbc70
Bhagyadilhara <bhagyadilhara123@gmail.com>
2023-03-19 18:00:50 +0530

    Add Author migrations & update db

---
+++ TaskAPI.DataAccess/Migrations/20230319122711_AuthorEntityAndData.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TaskAPI.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuthorEntityAndData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Todos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "FullName" },
+                values: new object[,]
+                {
+                    { 1, "Bhagya Dilhara" },
+                    { 2, "Mahima Induvara" },
+                    { 3, "Chanuka Supun" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "AuthorId", "Created", "Due" },
+                values: new object[] { 1, new DateTime(2023, 3, 19, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(402), new DateTime(2023, 3, 24, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(416) });
+
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "Id", "AuthorId", "Created", "Description", "Due", "Status", "Title" },
+                values: new object[,]
+                {
+                    { 2, 2, new DateTime(2023, 3, 19, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(423), "Test description 2", new DateTime(2023, 3, 24, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(424), 0, "Do office works - DB" },
+                    { 3, 3, new DateTime(2023, 3, 19, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(425), "Test description 3", new DateTime(2023, 3, 24, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(426), 0, "Do home works - DB" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_AuthorId",
+                table: "Todos",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_Authors_AuthorId",
+                table: "Todos",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_Authors_AuthorId",
+                table: "Todos");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Todos_AuthorId",
+                table: "Todos");
+
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Todos");
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 3, 19, 15, 55, 32, 167, DateTimeKind.Local).AddTicks(9123), new DateTime(2023, 3, 24, 15, 55, 32, 167, DateTimeKind.Local).AddTicks(9154) });
+        }
+    }
+}
