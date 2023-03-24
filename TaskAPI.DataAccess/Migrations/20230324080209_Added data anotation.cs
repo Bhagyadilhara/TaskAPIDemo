16329093b5e487212081feae884f72aec805119c
Bhagyadilhara <bhagyadilhara123@gmail.com>
2023-03-24 13:37:52 +0530

    Add data annotations

---
+++ TaskAPI.DataAccess/Migrations/20230324080209_Added data anotation.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskAPI.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addeddataanotation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Todos",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Todos",
+                type: "nvarchar(350)",
+                maxLength: 350,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Authors",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressNo",
+                table: "Authors",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Authors",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Authors",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "AddressNo", "City", "Street" },
+                values: new object[] { "55", "Maharagama", "Street 1" });
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "AddressNo", "City", "Street" },
+                values: new object[] { "100", "Pannipitiya", "Street 2" });
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "AddressNo", "City", "Street" },
+                values: new object[] { "64/2", "Kottawa", "Street 3" });
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 3, 24, 13, 32, 9, 44, DateTimeKind.Local).AddTicks(9659), new DateTime(2023, 3, 29, 13, 32, 9, 44, DateTimeKind.Local).AddTicks(9670) });
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 3, 24, 13, 32, 9, 44, DateTimeKind.Local).AddTicks(9676), new DateTime(2023, 3, 29, 13, 32, 9, 44, DateTimeKind.Local).AddTicks(9676) });
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 3, 24, 13, 32, 9, 44, DateTimeKind.Local).AddTicks(9678), new DateTime(2023, 3, 29, 13, 32, 9, 44, DateTimeKind.Local).AddTicks(9678) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressNo",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Authors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Todos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Todos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(350)",
+                oldMaxLength: 350,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 3, 19, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(402), new DateTime(2023, 3, 24, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(416) });
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 3, 19, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(423), new DateTime(2023, 3, 24, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(424) });
+
+            migrationBuilder.UpdateData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Created", "Due" },
+                values: new object[] { new DateTime(2023, 3, 19, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(425), new DateTime(2023, 3, 24, 17, 57, 11, 666, DateTimeKind.Local).AddTicks(426) });
+        }
+    }
+}
